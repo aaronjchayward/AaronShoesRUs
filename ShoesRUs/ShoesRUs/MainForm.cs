1115d413aa0bc2579aed2c3e448094e5a4cdba3c
Milixk <morgan.lovett@hotmail.co.uk>
2016-03-14 16:51:39 +0000

    first upload

---
+++ ShoesRUs/ShoesRUs/MainForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ShoesRUs
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            Startup su = new Startup();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Login login = new Login();
+            if(login.loggingIn(textBox1.Text, textBox2.Text) != -999)
+            {
+                MessageBox.Show("Logged in successfully!");
+            } else
+            {
+                MessageBox.Show("Login credentials not found!");
+            }
+        }
+
+    }
+}
---
+++ ShoesRUs/ShoesRUs/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ShoesRUs
+{
+    class Startup
+    {
+        //Run the startup functions.
+        public Startup()
+        {
+            DatabaseExist();
+        }
+
+        //Check if the database exists locally, if not, download the database to the same directory as the application.
+        private void DatabaseExist()
+        {
+            if (File.Exists(Application.StartupPath + @"\ShoesRUsDB.accdb") == false)
+            {
+                using (var client = new WebClient())
+                {
+                    client.DownloadFile("https://github.com/Milixk/shoesrus/blob/master/ShoesRUsDB.accdb?raw=true", Application.StartupPath + @"\ShoesRUsDB.accdb");
+                }
+            }
+            
+        }
+    }
+}
